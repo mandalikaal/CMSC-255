d783fe0f5dccef1bebd455510435eec784256d30
mandalikaal <69869301+mandalikaal@users.noreply.github.com>
2020-08-18 15:04:41 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*
+* Hello.java
+*
+* Akhila Mandalika
+* 8/18/2020
+* 255 Section C90
+*/
+
+public class Hello { 
+
+	public static void main(String[] args) {
+	
+		System.out.println("Akhila"); 
+		System.out.println("Hello.java"); 
+		System.out.println("My first project"); 
+		System.out.println("8/18/2020"); 
+		System.out.println("255 and C90");
+		System.out.println();
+		}
+	}
